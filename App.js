4e177689056eae013060baa9ccb7c565985b912d
NiranjanLearning1 <niranjanreddyanugu.learning@gmail.com>
2023-10-10 00:47:31 +0530

    Added Parcel features to list app.js

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
+import React from "react";
+import{ ReactDOM}  from "react-dom/client";
+
+/*
+***************************************Parcel features*************************************************
+*
+*HMR - Hot Module Replacement
+*File Watchers - written in c++
+*Bundling
+*Minifying 
+*Cleaning our code
+*Dev and Production build
+*Super fast build algorithm
+*Image Optimization
+*Caching While Development
+*Compression
+*Compatible with older versions of browsers
+*Https on Dev
+*Port number
+*Consistent Hashing Algorithm
+*Zero Config Bundler
+*Transitive Dependencies
+*
+*
+*/ 
+
 
 const heading = React.createElement("h1",{id:"heading", class:"h1"}, "Namste EveryOne");
 
--- README.md
+++ README.md
@@ -19,5 +19,8 @@ Chapter - 1
 Chapter - 2 Igniting the APP
 
 1.package-lock.json
-
+2.Parcel
+    -> npx parcel index.html (index.html is the entry point)
+    -> npx parcel build index.html (production build) 
+3.
 
